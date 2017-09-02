f8e840dc894aadd531fb2ff809d90fd36e9c96e5
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-02 17:06:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def take_a_number(deli, name)
 location = deli.index(name)
-puts "Welcome #{name}. You are number #{location} in line."
+puts "Welcome, #{name}. You are number #{location} in line."
 end
 
 def line(deli)
