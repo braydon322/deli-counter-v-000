f16fcf8e3d445b2231f585b752f8eb28749f37de
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-02 17:33:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,11 @@
 def take_a_number(deli, name)
 deli.push(name)
 location = deli.size
-if deli.size > 0
-  puts "Welcome, #{name}. You are number #{location} in line."
-else
-  puts "The line is currently empty."
-end
+  if deli.size > 0
+    puts "Welcome, #{name}. You are number #{location} in line."
+  else
+    puts "The line is currently empty."
+  end
 end
 
 def line(deli)
@@ -21,13 +21,13 @@ def line(deli)
   else
     puts "The line is currently empty."
   end
-
 end
 
 def now_serving(deli)
-if deli.size > 0
-  puts "Currently serving #{deli[0]}."
-  deli.shift
-else
-  puts "The line is currently empty."
+  if deli.size > 0
+    puts "Currently serving #{deli[0]}."
+    deli.shift
+  else
+    puts "The line is currently empty."
+  end
 end
