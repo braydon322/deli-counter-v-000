4207538f65f4bd40ef895ad6760986c6827c0d2c
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-02 16:42:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
-def take_a_number
+def take_a_number(katz_deli, name)
 end
-def take_a_number
+def take_a_number(other_deli, name)
 end
 def line(katz_deli)
 end
@@ -9,7 +9,7 @@ def line(other_deli)
 end
 def line(another_deli)
 end
-def now_serving
+def now_serving(katz_deli)
 end
-def now_serving
+def now_serving(other_deli)
 end
