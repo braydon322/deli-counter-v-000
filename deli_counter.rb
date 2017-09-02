9562929ebb602e0bdff79978eb7a44733fd0ee77
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-02 17:13:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 # Write your code here.
 def take_a_number(deli, name)
-location = deli
+deli.push(name)
+location = deli.size + 1
 puts "Welcome, #{name}. You are number #{location} in line."
 end
 
