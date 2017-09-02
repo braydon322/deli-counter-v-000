00e6e20a9c49a8762bc79e97f211c38dc3fcbd6e
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-02 16:47:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,22 +1,7 @@
 # Write your code here.
-def take_a_number(katz_deli, name)
-
+def take_a_number(deli, name)
 end
-def take_a_number(other_deli, name)
+def line(deli)
 end
-def line(katz_deli)
-  if katz_deli.size == 0
-    puts "The line is currently empty."
-  end
-end
-def line(other_deli)
-  if katz_deli.size == 0
-    puts "The line is currently empty."
-  end
-end
-def line(another_deli)
-end
-def now_serving(katz_deli)
-end
-def now_serving(other_deli)
+def now_serving(deli)
 end
